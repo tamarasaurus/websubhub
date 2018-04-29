1b052654f6605b29aa1e40a706e812b67cc23420
tamarasaurus <tamara@robichet.com>
2018-04-29 17:44:07 +0200

    fix tests

--- lib/websubhub_web/controllers/page_controller.ex
+++ lib/websubhub_web/controllers/page_controller.ex
@@ -2,9 +2,6 @@ defmodule WebsubhubWeb.PageController do
   use WebsubhubWeb, :controller
 
   def index(conn, _params) do
-    # return allowed methods in hal format
-    # subscribe with required hub params
-    # unsubscribe with subscription_id
-    text conn, "hello websubhub"
+    text conn, "endpoints"
   end
 end
